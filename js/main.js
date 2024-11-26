a2c722057a8a6c8189ebab7c252573096bc70470
a.naufal681 <a.naufal681@gmail.com>
2024-11-26 04:07:26 +0000

    first commit

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@
 
     // Countdown Timer
     function countDownTimer() {	
-        var endTime = new Date("26 November 2024 10:00:00 GMT+00:00");
+        var endTime = new Date("03 December 2024 10:00:00 GMT+00:00");
         endTime = (Date.parse(endTime) / 1000);
 
         var now = new Date();
